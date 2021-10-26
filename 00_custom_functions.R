01313b6bb1262b7aad195499531bc3fe9339d1ff
James Cutler <jamescutler@stucph1jcutler.hsc.net.ou.edu>
2021-10-26 11:57:10 -0500

    Added maps_and_web_scraping.R and some updates.

--- 00_custom_functions.R
+++ 00_custom_functions.R
@@ -2,7 +2,7 @@
 # Custom functions for R Basics ----
 
 # Libraries
-# pacman::p_load(tidyverse,tidyquant,dygraphs)
+# pacman::p_load(tidyverse,tidyquant,dygraphs,rvest)
 # Try it out
 # c("AAPL","MSFT","NFLX") %>% 
 #   tq_get(from = "2015-01-01") %>% 
@@ -23,9 +23,43 @@ stock_tbl_to_xts <- function(your_stock_tbl,stock_symbol){
 #   dyRangeSelector()
 
 
+my_scrape <- function(your_url,integer_vector,the_node){
+  integer_vector %>% 
+    map_df(.f = function(X){
+      your_url %>% 
+        httr::GET(config = httr::config(ssl_verifypeer = FALSE)) %>% 
+        read_html() %>% 
+        html_nodes(the_node) %>% 
+        .[[X]] %>% 
+        # Listify, then unlistify
+        xml2::as_list() %>% unlist() %>% 
+        as_tibble() #%>% 
+      # Remove debris rows 
+      # filter(! (str_detect(value,"^\\,") | str_detect(value,"^\n")) )
+    }) 
+}
 
 
-
+my_jitter_coords <- function(coords_tbl,sd=.01){
+  coords_tbl %>% unite("location",c(lat,lng),remove = F) -> coords_tbl
+  
+  coords_tbl %>% 
+    count(location) %>% 
+    filter(n > 1) -> coords_identical
+  
+  coords_tbl %>% 
+    filter(location %in% coords_identical$location) %>% 
+    rowwise() %>% 
+    mutate(
+      lat = lat + rnorm(1,sd=sd),
+      lng = lng + rnorm(1,sd=sd)
+    ) %>% 
+    bind_rows(
+      coords_tbl %>% filter(!(location %in% coords_identical$location))
+    ) -> coords_tbl
+  
+  return(coords_tbl)
+}
 
 
 
---
+++ maps_and_web_scraping.R
@@ -0,0 +1,134 @@
+
+
+# maps_and_web_scraping ----
+
+# This script shows you how to make both a static as well as an interactive simple map in
+# R, and also how to web scrape the information you need in the first place to plot on 
+# the map.
+
+# Libraries ----
+pacman::p_load(
+  tidyverse,
+  tidyquant,
+  scales,
+  leaflet,
+  rvest,
+  tmap,
+  googleway
+)
+
+
+# Custom functions ----
+source("00_custom_functions.R")
+
+
+
+# 0.0 - Pretend you don't have this ----
+school_coords_tbl <- read_rds("00_data/OK_county_high_schools_coords.rds")
+
+
+
+# 1.0 - Scrape school data ----
+"https://en.wikipedia.org/wiki/List_of_high_schools_in_Oklahoma" %>% 
+  my_scrape(61:64,"ul") %>% 
+  filter(! (str_detect(value,"^\\,") | str_detect(value,"^\n")) ) -> school_1_tbl
+
+"https://en.wikipedia.org/wiki/List_of_high_schools_in_Oklahoma" %>% 
+  my_scrape(394:417,"li") %>% 
+  filter(!(str_detect(value,"^\\,"))) -> school_2_tbl
+
+school_1_tbl
+school_2_tbl
+
+# Combine the two scraped tibbles into new tibble, then delete them
+school_tbl <- bind_rows(school_1_tbl,school_2_tbl)
+rm(school_1_tbl,school_2_tbl)
+
+# Add 'Oklahoma' to school names to help Google find them
+school_tbl %>% 
+  set_names("school") %>% 
+  mutate(school = str_c(school,", Oklahoma",sep="")) -> school_tbl
+
+school_tbl
+
+
+
+# 2.0 - Geocode schools ----
+## WARNING ----
+# WARNING: THIS WILL NOT WORK UNLESS YOU HAVE A GOOGLE API KEY!
+school_tbl %>% 
+  pull(school) %>% 
+  map(.f = function(X){
+    res <- google_geocode(X,key = "YOUR KEY GOES HERE")
+    df <- res$results
+    location <- df$geometry$location
+    my_tbl <- bind_cols(
+      # Col. 1, Col. 2
+      location, str_glue("{X}")
+    )
+    return(my_tbl)
+  }) -> school_coords_tbl
+
+school_coords_tbl
+
+## Clean up ----
+school_coords_tbl %>% bind_rows() %>% as_tibble() %>% 
+  select(lat:...3) %>%       # Select only the columns we want
+  rename(school = ...3) %>%  # Rename the column name from '...3' to 'school'
+  mutate(                    # Now that we don't need the 'Oklahoma', get rid of it
+    school = str_remove(school,"\\, Oklahoma") 
+  ) -> school_coords_tbl
+
+# More cleanup - Remove duplicates (e.g. Bethany High)
+# Some good ole base R code! (still useful)
+school_coords_tbl %>% 
+  .[-which(duplicated(school_coords_tbl$school)),] -> school_coords_tbl
+
+
+
+# 3.0 - Map stuff ----
+## html labels for map ----
+school_coords_tbl$labels <- pmap(
+  list(school_coords_tbl$school),
+  function(first){
+    htmltools::HTML(str_glue("{first}"))
+  }
+)
+
+## Leaflet map! ----
+school_coords_tbl %>% 
+  leaflet() %>% 
+  addProviderTiles("CartoDB.Voyager") %>% 
+  setView(lng = -97.5,lat = 35.5, zoom = 10) %>% 
+  addCircleMarkers(
+    label = ~labels,
+    lat = ~lat,
+    lng = ~lng,
+    radius = 7
+  )
+
+## Jitter some points ----
+# (Some coordinate pairs are literally identical to each other.)
+school_coords_tbl %>% my_jitter_coords(sd=.001) -> school_jitter_tbl
+
+### Map again ----
+school_jitter_tbl %>% 
+  leaflet() %>% 
+  addProviderTiles("CartoDB.Voyager") %>% 
+  setView(lng = -97.5,lat = 35.5, zoom = 10) %>% 
+  addCircleMarkers(
+    label = ~labels,
+    lat = ~lat,
+    lng = ~lng,
+    radius = 7
+  )
+
+
+
+# 4.0 - SAVE your geocoded data! ----
+# (It makes it so you don't have to geocode again!)
+# Obviously, this step has already been done (the data is already in 00_data/).
+# school_coords_tbl %>% write_rds("00_data/OK_county_high_schools_coords.rds")
+
+
+
