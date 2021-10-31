31f93bf01ad4046b43b3bfc47303931e25b94b28
James Cutler <jamescutler@stucph1jcutler.attlocal.net>
2021-10-30 21:35:11 -0500

    May have messed up on this git push

--- test.R
+++ test.R
@@ -2,3 +2,16 @@ yesterday <- function(){
   x <- Sys.Date() - 1
   return(x)
 }
+
+
+
+
+pooled_variance <- function(n1,n2,s1,s2){
+  ( (n1 - 1)*s1^2   +   (n2 - 1)*s2^2 ) / 
+    (n1 + n2 - 2)
+}
+
+# pooled_variance(80,22,15.3,18.2)
+
+
+
